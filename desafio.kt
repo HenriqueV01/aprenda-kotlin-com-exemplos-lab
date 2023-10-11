3cad94ba807ffb5973da37717b9dfb41c333cb71
Henrique Venancio <henrique.unibratec01@gmail.com>
2023-10-11 11:19:02 -0300

    Implementação da matricula de usuario na formação.

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,8 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, var idade: Int)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -10,12 +10,29 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuarios: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.addAll(usuarios)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Kotlin")
+    val conteudo2 = ConteudoEducacional("Java")
+    
+    var conteudos: MutableList<ConteudoEducacional> = mutableListOf(conteudo1, conteudo2)
+    
+    var formacao = Formacao("Programador", conteudos)
+    
+    var usuario1 = Usuario("Henrique", 31)
+    var usuario2 = Usuario("João", 21)
+    var usuario3 = Usuario("Marcos", 29)
+    
+    formacao.matricular(usuario1, usuario2, usuario3)
+    println("Usuario(s) matriculado(s): ${formacao.inscritos.size}")
+    for(usuario in formacao.inscritos){
+        println("Formacao: ${formacao.nome} \r\n Aluno: ${usuario.nome} \r\n Idade: ${usuario.idade}")
+    }    
 }
